a2650e8837622d0eb85e30b511b10d5f15bccae2
opersian_rg <olena.persianova@raccoongang.com>
2020-12-30 11:35:23 +0200

    FLS-499: Sort a group of courses per ids specific order

--- search/utils.py
+++ search/utils.py
@@ -1,6 +1,21 @@
 """ Utility classes to support others """
 import importlib
 import collections
+import logging
+
+
+log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+# The order of courses is important
+COURSES_ORDER = [
+    "course-v1:Lakeside+TE101+2019",  # The Essentials of Trauma - The Opioid Crisis
+    "course-v1:NeuroLogic+NL101+2020Q2",  # The Trauma-Informed Classroom
+    "course-v1:Neurologic+NL105+2021_T1",  # Resilience: Increasing Students' Tolerance for Stress
+    "course-v1:LakesideGlobalInstitute+2000+2020_T1",  # Enhancing Trauma Awareness
+    "course-v1:Lakeside+PRE1101+2020",  # PRESENCE - Introductory Track
+    "course-v1:Lakeside+PRE1101+2020_T2",  # PRESENCE-Leadership Track
+    "course-v1:Lakeside+PRE1103+2029_T2",  # PRESENCE - Clinical Track
+]
 
 
 def _load_class(class_path, default):
@@ -64,3 +79,84 @@ class DateRange(ValueRange):
     def lower_string(self):
         """ use isoformat for _lower date's string format """
         return self._lower.isoformat()
+
+
+def sort_courses_by_hardcoded_ids_order(data):
+    """
+    Sort a batch of courses as per the course ids hardcoded order.
+
+    "programs" value will be updated, in particular, a respective batch of courses,
+    where the courses of interest appear.
+
+    Sequence of hardcoded courses will go first in its courses group.
+
+    NOTE: consider separating out results parsing from courses batch reordering.
+
+    Arguments:
+        data (dict): nested search results. Example:
+        ```
+        {
+            'programs_total': 15,
+            'programs': [
+                {
+                    'subtitle': u'',
+                    'title': u'Lakeside Demo',
+                    'courses': [
+                        {
+                            u'_type': u'course_info',
+                            'score': 1.0,
+                            u'_index': u'courseware_index',
+                            u'_score': 1.0,
+                            u'_id': u'course-v1:honor01+101+2019',
+                            'data': {
+                                u'category': [u'Category 1 1 1 1 1'],
+                                u'product': False,
+                                u'end': u'2022-01-01T00:00:00+00:00',
+                                u'modes': [u'honor'],
+                                # ...
+                            }
+                            # ...
+                        }
+                    ],
+                },
+            ],
+            # ...  'facets', 'took', 'results', 'total', 'max_score' ...
+        }
+        ```
+
+    Returns:
+        data with re-ordered courses in a certain group (dict)
+    """
+    programs = data.get("programs")
+    unsorted_first_courses_data = {}
+
+    first_courses = []
+    other_courses = []
+    # To remember a program containing courses of interest
+    program_index = None
+
+    # Separate out all the courses, excluding the ones configured in COURSES_ORDER
+    # Also, remember the program index containing the courses from COURSES_ORDER
+    for i, program in enumerate(programs):
+        p_courses = program.get("courses")
+        for p_course in p_courses:
+            course_key = p_course.get("_id")
+            if course_key in COURSES_ORDER:
+                program_index = i
+                unsorted_first_courses_data[course_key] = p_course
+            elif program_index is not None:
+                other_courses.append(p_course)
+        # Course ids are unique, so courses with same ids can't appear in other programs
+        if program_index is not None:
+            break
+
+    # Populate the courses of interest i.e. the ones we reorder
+    for config_key in COURSES_ORDER:
+        course = unsorted_first_courses_data.get(config_key)
+        first_courses.append(course)
+
+    sorted_data = dict(data)
+    if first_courses and other_courses:
+        sorted_data["programs"][program_index]["courses"] = first_courses + other_courses
+
+    return sorted_data
